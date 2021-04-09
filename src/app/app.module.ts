44cd64ab31f0b2a9bb9cd7ee67986946c4901189
Andrea Scrimieri <74497321+screamieri@users.noreply.github.com>
2021-04-09 15:04:19 +0200

    Started Setting Up Booklist App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookItemComponent } from './book-list/book-item/book-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookListComponent,
+    BookItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
